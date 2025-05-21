17829dcf4cc4a06e17fe894d612dbf209a553e75
jeff <jeffreyr@securitydept.com>
2025-05-21 14:39:11 -0400

    ops

--- src/python/corpusflowai/sources/__init__.py
+++ src/python/corpusflowai/sources/__init__.py
@@ -8,5 +8,5 @@ from .document import DocumentType, DocumentMetadata, Document
 from .exceptions import DocumentSourceException
 from .google import GoogleDocsSource
 from .local import LocalFileSystemSource
-from .office365 import Office365Source
-from .s3 import S3Source
+# from .office365 import Office365Source
+# from .s3 import S3Source
